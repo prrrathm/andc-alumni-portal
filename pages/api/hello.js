eea9aff75576fa8d2e9732683b95908ba9752799
Pratham Sharma <prrrathm@gmail.com>
2022-05-16 19:56:22 +0530

    prisma added

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,3 +3,10 @@
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+
+// require('dotenv').config();
+// const mysql = require('mysql2');
+// const connection = mysql.createConnection(process.env.DATABASE_URL);
+// console.log('Connected to PlanetScale!');
+// connection.end();
